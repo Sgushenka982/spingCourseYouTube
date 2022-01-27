b7337278317b56daf30d40eed716bf5358215af3
Sgushenka982 <73597696+Sgushenka982@users.noreply.github.com>
2022-01-27 19:33:50 +0300

    8 lesson

--- src/main/java/ru/gushchinDA/springCourseYouTube/ClassicalMusic.java
+++ src/main/java/ru/gushchinDA/springCourseYouTube/ClassicalMusic.java
@@ -1,6 +1,21 @@
 package ru.gushchinDA.springCourseYouTube;
 
 public class ClassicalMusic implements Music{
+
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Hungurian rapsody";
