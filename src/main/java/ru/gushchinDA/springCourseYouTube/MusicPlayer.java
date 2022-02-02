037d101340774424122810d94c0b0d46ca5a6c53
Sgushenka982 <73597696+Sgushenka982@users.noreply.github.com>
2022-02-02 23:50:02 +0300

    11 lesson @Qualifier

--- src/main/java/ru/gushchinDA/springCourseYouTube/MusicPlayer.java
+++ src/main/java/ru/gushchinDA/springCourseYouTube/MusicPlayer.java
@@ -1,22 +1,25 @@
 package ru.gushchinDA.springCourseYouTube;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-    private RapMusic rapMusic;
+    private Music music1;
+    private Music music2;
+    private Music music3;
 
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic, RapMusic rapMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-        this.rapMusic = rapMusic;
+    public MusicPlayer(@Qualifier("classicalMusic") Music music1,
+                       @Qualifier("rockMusic") Music music2,
+                       @Qualifier("rapMusic") Music music3 ) {
+        this.music1 = music1;
+        this.music2 = music2;
+        this.music3 = music3;
     }
 
     public String playMusic(){
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music1.getSong() + ", then " + music2.getSong() + ", then " + music3.getSong();
     }
 }
